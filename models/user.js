87b48e185aeb28d389b4d0129b1e012fd090cc4d
Vita Chen <39967407+vita-wanchenchen@users.noreply.github.com>
2018-12-01 09:21:44 -0500

    setup models

---
+++ models/user.js
@@ -0,0 +1,19 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  }),
+  
+    User.associate = function(models) {
+      User.hasMany(models.Post, {
+        onDelete: "cascade"
+      });
+    };
+  
+    return Author;
+  };
