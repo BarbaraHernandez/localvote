7419db6d906504c07d2496f3a44e75bcf534304b
Vita Chen <39967407+vita-wanchenchen@users.noreply.github.com>
2018-12-06 20:24:11 -0500

    backup before pull master branch

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-
+/*
 // Passport
 authInit(passport);
 authFacebookStrategy(passport);
