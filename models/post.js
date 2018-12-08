cadbe25f2ccec8c5c318e1295de4d2de0acc508b
carolyn <carolynbbernier@gmail.com>
2018-12-08 12:34:59 -0500

    fixed merge issue

--- models/post.js
+++ models/post.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = function(sequelize, DataTypes) {
   var Post = sequelize.define("Post", {
     title: {
@@ -30,32 +29,3 @@ module.exports = function(sequelize, DataTypes) {
 
   return Post;
 };
-=======
-module.exports = function(sequelize, DataTypes) {
-  var Post = sequelize.define("Post", {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    policyDetail: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    category: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  });
-
-  Post.associate = function(models) {
-    Post.belongsTo(models.Account, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-  };
-
-  return Post;
-};
->>>>>>> master
